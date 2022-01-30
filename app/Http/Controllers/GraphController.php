61c725428b6416e2d3f655cc7db9c0f97e10bc9b
TopBor <dborovyk86@gmail.com>
2022-01-30 13:43:59 +0200

    Get graphs to concrete date

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -3,31 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\Enrolment;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class GraphController extends Controller
 {
-    public function getEnrolments()
+    public function getEnrolments(Request $request)
     {
-        return DB::table('mdl_user_enrolments')
-            ->select(DB::raw('count(*) as count'),'timecreated')
-            ->groupBy('timecreated')
-            ->get();
+        if($request->get('date')){
+            return DB::table('mdl_user_enrolments')
+                ->select(DB::raw('count(*) as count'),'timecreated')
+                ->where('timecreated', $request->get('date'))
+                ->groupBy('timecreated')
+                ->get();
+        }
+        else{
+            return DB::table('mdl_user_enrolments')
+                ->select(DB::raw('count(*) as count'),'timecreated')
+                ->groupBy('timecreated')
+                ->get();
+        }
     }
 
-    public function getCompletions()
+    public function getCompletions(Request $request)
     {
-        return DB::table('mdl_course_completions')
-            ->select(DB::raw('count(*) as count'),'timecompleted')
-            ->groupBy('timecompleted')
-            ->get();
+        if($request->get('date')){
+            return DB::table('mdl_course_completions')
+                ->select(DB::raw('count(*) as count'),'timecompleted')
+                ->where('timecompleted', $request->get('date'))
+                ->groupBy('timecompleted')
+                ->get();
+        }
+        else{
+            return DB::table('mdl_course_completions')
+                ->select(DB::raw('count(*) as count'),'timecompleted')
+                ->groupBy('timecompleted')
+                ->get();
+        }
     }
 
-    public function getEnrolmentMethods()
+    public function getEnrolmentMethods(Request $request)
     {
-        return DB::table('mdl_enrol')
-            ->select(DB::raw('count(*) as count'),'enrol')
-            ->groupBy('enrol')
-            ->get();
+        if($request->get('date')){
+            return DB::table('mdl_enrol')
+                ->select(DB::raw('count(*) as count'),'enrol')
+                ->where('timecreated', $request->get('date'))
+                ->groupBy('enrol')
+                ->get();
+        }
+        else{
+            return DB::table('mdl_enrol')
+                ->select(DB::raw('count(*) as count'),'enrol')
+                ->groupBy('enrol')
+                ->get();
+        }
     }
 }
